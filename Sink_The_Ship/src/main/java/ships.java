867bb29d3a746b6857e5ff5362630a70e8f2c487
YuSuF_Abdelfattah <97110015+JOEGREEEN@users.noreply.github.com>
2022-11-11 01:25:42 +0200

    Terminal Sink The Ship Game

---
+++ Sink_The_Ship/src/main/java/ships.java
@@ -0,0 +1,16 @@
+//ღ(¯`◕‿◕´¯) ♫ ♪ ♫ YuSuF ♫ ♪ ♫ (¯`◕‿◕´¯)ღ
+
+public class ships {
+
+    String shipName;
+    int shipLength; // random number + that number
+    int shipNo;
+    boolean dead;
+
+    ships(String shipName, int shipLength, int shipNo) {
+        this.shipName = shipName;
+        this.shipLength = shipLength;
+        this.shipNo = shipNo;
+    }
+
+}
